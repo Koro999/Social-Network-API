7e6b539c2b5c02839672e2b161d81f5d759e49b4
Koro999 <chris.dang93@hotmail.com>
2023-08-30 16:54:09 -0400

    added some more comments, renamed index.js to server.js

--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 // Schema to create User model
+//the model has four fields 
 const userSchema = new mongoose.Schema(
   {
     username: {
@@ -17,40 +18,21 @@ const userSchema = new mongoose.Schema(
     },
     thoughts: [{
       type: mongoose.Schema.Types.ObjectId,
-      ref: 'Thought'
+      ref: 'Thought' //reference the Thought model
     }],
     friends: [{
       type: mongoose.Schema.Types.ObjectId,
-      ref: 'User'
+      ref: 'User' //reference the User model (self-reference)
     }]
   });
-  
-/* use of virtuals 
-  {
-    // Mongoose supports two Schema options to transform Objects after querying MongoDb: toJSON and toObject.
-    // Here we are indicating that we want virtuals to be included with our response, overriding the default behavior
-    toJSON: {
-      virtuals: true,
-    },
-    id: false,
-  }
-);*/
 
-// Create a virtual property `fullName` that gets and sets the user's full name
-userSchema
-  .virtual('fullName')
-  // Getter
-  .get(function () {
-    return `${this.first} ${this.last}`;
-  })
-  // Setter to set the first and last name
-  .set(function (v) {
-    const first = v.split(' ')[0];
-    const last = v.split(' ')[1];
-    this.set({ first, last });
-  });
+// Define the virtual property
+userSchema.virtual('friendCount').get(function() {
+  //return friends length, refers to the schema itself 
+  return this.friends.length;
+});
 
-// Initialize our User model
-const User = model('user', userSchema);
+// Initialize our User model (collections)
+const User = mongoose.model('user', userSchema);
 
 module.exports = User;
